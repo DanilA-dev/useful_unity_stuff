395ac94e20046821c97902d797a04bed466ebcda
DanilA-dev <78906212+DanilA-dev@users.noreply.github.com>
2024-04-27 16:54:20 +0300

    upd

---
+++ Extensions/TransformExtensions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace Extensions
+{
+    public static class TransformExtensions
+    {
+        public static void RotateTowardsDirection(this Transform owner, Vector3 target, Vector3 axis, float speed)
+        {
+            var dir = (target - owner.position);
+            Quaternion look = Quaternion.LookRotation(dir, axis);
+            owner.transform.rotation = Quaternion.RotateTowards(owner.transform.rotation, look, speed * Time.deltaTime);
+            
+        }
+        
+        public static void RotateTowards(this Transform owner, Vector3 target, Vector3 axis, float speed)
+        {
+            Quaternion look = Quaternion.LookRotation(target, axis);
+            owner.transform.rotation = Quaternion.RotateTowards(owner.transform.rotation, look, speed * Time.deltaTime);
+            
+        }
+    }
+}
